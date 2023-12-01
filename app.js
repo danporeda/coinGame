eb34188611abacc0ae7a461d87e1ee13133c57f1
Dan <danporeda@yahoo.com>
2023-12-01 17:58:32 -0500

    completed first version of coin chase demo

--- app.js
+++ app.js
@@ -11,7 +11,37 @@ function isTouching(a, b) {
 }
 
 const avatar = document.querySelector('#player');
+const coin = document.querySelector('#coin');
 
 window.addEventListener('keyup', function(e) {
-    console.log(e.key);
+    if (e.key === 'ArrowDown' || e.key === 'Down') {
+        const currTop = extractPos(avatar.style.top);
+        avatar.style.top = `${currTop + 50}px`
+    } else if (e.key === 'ArrowUp' || e.key === 'Up'){
+        const currTop = extractPos(avatar.style.top);
+        avatar.style.top = `${currTop - 50}px`
+    } else if (e.key === 'ArrowRight' || e.key === 'Right'){
+        const currLeft = extractPos(avatar.style.left);
+        avatar.style.left = `${currLeft + 50}px`
+        avatar.style.transform = 'scale(1,1)';
+    } else if (e.key === 'ArrowLeft' || e.key === 'Left'){
+        const currLeft = extractPos(avatar.style.left);
+        avatar.style.left = `${currLeft - 50}px`
+        avatar.style.transform = 'scale(-1,1)';
+    }
+    if(isTouching(avatar,coin)) moveCoin();
 });
+
+const extractPos = (pos) => {
+    if(!pos) return 100;
+    return parseInt(pos.slice(0,-2));
+}
+
+const moveCoin = () => {
+    const y = Math.floor(Math.random() * window.innerHeight);
+    const x = Math.floor(Math.random() * window.innerWidth);
+    coin.style.top = `${y}px`;
+    coin.style.left = `${x}px`;
+}
+
+moveCoin();
